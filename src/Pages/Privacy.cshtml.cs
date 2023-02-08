8ca1e610052c7b6ebfd9760384ade7bd1b650d26
dsrocha01 <dsrocha01@gmail.com>
2023-02-07 21:06:46 -0300

    Initial Commit

---
+++ src/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace myfinance_web_netcore.Pages;
+
+public class PrivacyModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
